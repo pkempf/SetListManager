0149c17346554d1fd533a0e6ff5693e1807d287f
Peter Kempf <peterckempf@gmail.com>
2020-12-14 16:51:00 -0600

    fixed bug: lyrics should have been optional when updating songs

--- forms.py
+++ forms.py
@@ -75,9 +75,7 @@ class SongUpdateForm(FlaskForm):
 
     title = StringField("Song title:", validators=[DataRequired()])
     artist = StringField("Artist:", validators=[DataRequired()])
-    lyrics = TextAreaField(
-        "Lyrics:", validators=[DataRequired()], render_kw={"rows": "10"}
-    )
+    lyrics = TextAreaField("Lyrics:", validators=[Optional()], render_kw={"rows": "10"})
     submit = SubmitField("Update Song", render_kw={"action": "post"})
 
 
